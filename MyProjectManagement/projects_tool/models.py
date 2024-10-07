b42b983a2cd96d5bf062491ee39ecd74578ee8ad
Eva Harboe <evharboe@gmail.com>
2024-10-07 17:51:01 +0100

    models done

--- MyProjectManagement/projects_tool/models.py
+++ MyProjectManagement/projects_tool/models.py
@@ -1,49 +1,78 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 import re
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+# custom validator
+def division_by_five(value):
+    if value % 5 != 0:
+        raise ValidationError(
+            ('The value %(value)s must be divisible by 5.'),
+            params={'value': value},
+        )
 
 class Project(models.Model):
     title = models.CharField(max_length=64, unique=True, null=False, blank=False)
-    description = models.TextField(max_length=256)
-    image = models.ImageField(upload_to='media')
+    description = models.TextField(max_length=256, blank=True)
+    image = models.ImageField(upload_to='media/', blank=True)
     slug = models.SlugField(max_length=64, unique=True, null=False, blank=False)
-    # deleting a project destroys its boards, lists, tasks, and labels*
-    # a project can exist without any boards, lists, or labels
+
+    def __str__(self):
+        return self.title
 
 class Board(models.Model):
-    # a project, which must exist*
-    title = models.CharField(max_length=64, unique=True) # unique within a project not necessaraly across other projects
-    # boards can exist without lists
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64, unique=True)
+
+    class Meta:
+        unique_together = ('project', 'title')
+
+    def __str__(self):
+        return self.title
 
 class List(models.Model):
-    # a board, which must exist*
-    title = models.CharField(max_length=64, unique=True, null=False, blank=False) # unique within a board not necessaraly across other boards
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64, unique=True, null=False, blank=False)
     position = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('board', 'title')
+        
+    def __str__(self):
+        return self.title
 
 class Task(models.Model):
     task_no = models.IntegerField(primary_key=True)
-    # a list, which must exist*
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
     title = models.CharField(max_length=64, null=False, blank=False)
-    description = models.TextField(max_length=512)
-    priority = models.CharField(max_length=1, choices=[('L', 'Low'), ('M', 'Medium'), ('H', 'High')], default='M') # is this an enum?
-    story_points = models.PositiveIntegerField(min_value=0, max_value=100, divisable_by=5)
+    description = models.TextField(max_length=512, blank=True, null=True)
+    priority = models.CharField(max_length=1, choices=[('L', 'Low'), ('M', 'Medium'), ('H', 'High')], default='M')
+    story_points = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100),
+            division_by_five
+        ]
+    )
     labels = models.ManyToManyField('Label')
 
+
 class Label(models.Model):
-    title = models.CharField(max_length=32, unique=True, null=False) # unique within project only
-    colour = models.CharField(max_length=7) # hex color code
-    # a project, which must exist, and zero to many tasks*
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=32, null=False)
+    colour = models.CharField(max_length=7)
+
+    class Meta:
+        unique_together = ('project', 'title')
 
     def is_valid_hex(self, colour):
         return bool(re.match(r'^#([A-Fa-f0-9]{6})$', colour))
 
     def clean(self):
-        # Call is_valid_hex with self.colour
         if not self.is_valid_hex(self.colour):
             raise ValidationError('Invalid hex color code')
         
     def save(self, *args, **kwargs):
         self.clean()
         super().save(*args, **kwargs)
+
